e135aa7d21cb4566abc08e88b7f28fe4f0899554
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2020-12-29 23:31:27 +0530

    Added cypress test to test deleting a blog
    Completed Exercise 5.21

--- cypress/integration/bloglist.spec.js
+++ cypress/integration/bloglist.spec.js
@@ -14,25 +14,39 @@ describe('Blog app', function () {
   });
 
   it('Login form is shown', function () {
-    cy.get('#loginForm').should('be.visible');
-    cy.get('#loginForm').should('contain', 'username');
-    cy.get('#loginForm').get('button').should('contain', 'login');
+    cy.get('#loginForm')
+      .should('be.visible');
+    cy.get('#loginForm')
+      .should('contain', 'username');
+    cy.get('#loginForm')
+      .get('button')
+      .should('contain', 'login');
   });
 
   describe('Login', function () {
     it('succeeds with correct credentials', function () {
-      cy.get('input#username').type(user.username);
-      cy.get('input#password').type(user.password);
-      cy.get('#loginButton').click();
+      cy.get('input#username')
+        .type(user.username);
+      cy.get('input#password')
+        .type(user.password);
+      cy.get('#loginButton')
+        .click();
       cy.contains('Jayanth PSY');
-      cy.get('button').contains('login').should('not.exist');
+      cy.get('button')
+        .contains('login')
+        .should('not.exist');
     });
 
     it('fails with wrong credentials', function () {
-      cy.get('input#username').type(user.username);
-      cy.get('input#password').type('wrong-password');
-      cy.get('button').contains('login').click();
-      cy.contains('Invalid username or password').should('have.css', 'color', 'rgb(255, 0, 0)');
+      cy.get('input#username')
+        .type(user.username);
+      cy.get('input#password')
+        .type('wrong-password');
+      cy.get('button')
+        .contains('login')
+        .click();
+      cy.contains('Invalid username or password')
+        .should('have.css', 'color', 'rgb(255, 0, 0)');
     });
   });
 
@@ -46,39 +60,94 @@ describe('Blog app', function () {
     });
 
     it('A blog can be created', function () {
-      cy.contains('new blog').click();
-      cy.get('input#title').type('First Blog');
-      cy.get('input#author').type('Jay PSY');
-      cy.get('input#url').type('http://localhost:3652/first-blog');
-      cy.get('button').contains('create').click();
-      cy.get('.blog').contains('First Blog');
+      cy.contains('new blog')
+        .click();
+      cy.get('input#title')
+        .type('First Blog');
+      cy.get('input#author')
+        .type('Jay PSY');
+      cy.get('input#url')
+        .type('http://localhost:3652/first-blog');
+      cy.get('button')
+        .contains('create')
+        .click();
+      cy.get('.blog')
+        .contains('First Blog');
     });
 
-    it('created blog can be liked', function () {
-      cy.contains('new blog').click();
-      cy.get('input#title').type('First Blog');
-      cy.get('input#author').type('Jay PSY');
-      cy.get('input#url').type('http://localhost:3652/first-blog');
-      cy.get('button').contains('create').click();
-      cy.get('.blog').contains('First Blog').siblings().get('button')
-        .contains('view')
-        .click();
+    describe('after creating a blog', function () {
+      const blog = {
+        title: 'First Blog',
+        author: 'Jay PSY',
+        url: 'http://localhost:3652/first-blog',
+      };
 
-      // get initial likes
-      let likesCount;
-      cy.get('.blogLikes').should(function ($div) {
-        const text = $div.text();
-        likesCount = parseInt(text.match(/\d+/)[0], 10);
+      beforeEach(function () {
+        cy.contains('new blog')
+          .click();
+        cy.get('input#title')
+          .type(blog.title);
+        cy.get('input#author')
+          .type(blog.author);
+        cy.get('input#url')
+          .type(blog.url);
+        cy.get('button')
+          .contains('create')
+          .click();
+        cy.get('.blog')
+          .contains('First Blog');
       });
 
-      // click like button
-      cy.get('.blog').contains('First Blog').siblings().get('button')
-        .contains('like')
-        .click();
+      it('can be liked', function () {
+        cy.get('.blog')
+          .contains('First Blog')
+          .siblings()
+          .get('button')
+          .contains('view')
+          .click();
 
-      cy.get('.blogLikes').should(function ($div) {
-        const text = $div.text();
-        expect(text).to.include(likesCount + 1);
+        // get initial likes
+        let likesCount;
+        cy.get('.blogLikes')
+          .should(function ($div) {
+            const text = $div.text();
+            likesCount = parseInt(text.match(/\d+/)[0], 10);
+          });
+
+        // click like button
+        cy.get('.blog')
+          .contains('First Blog')
+          .siblings()
+          .get('button')
+          .contains('like')
+          .click();
+
+        cy.get('.blogLikes')
+          .should(function ($div) {
+            const text = $div.text();
+            expect(text)
+              .to
+              .include(likesCount + 1);
+          });
+      });
+
+      it('can be deleted', function () {
+        cy.get('.blog')
+          .contains('First Blog')
+          .siblings()
+          .get('button')
+          .contains('view')
+          .click();
+
+        cy.get('.blog')
+          .contains('First Blog')
+          .siblings()
+          .get('button')
+          .contains('delete')
+          .click();
+
+        cy.get('.blog').contains('First Blog')
+          .should('not.exist');
       });
     });
   });
