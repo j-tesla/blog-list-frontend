8b34624ad98161bc1fc73cca4e8de40504bc82b5
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2021-06-16 13:33:10 +0530

    styled User.jsx with material ui

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,34 +1,68 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import {
+  Card, CardContent, CardHeader, List, ListItem, ListItemText, makeStyles, Typography,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(5),
+    margin: theme.spacing(2),
+    '& > *': {
+      margin: theme.spacing(2),
+    },
+  },
+  boldText: {
+    textDecoration: 'bold',
+  },
+}));
 
 const User = () => {
   const { id } = useParams();
+  const classes = useStyles();
+  const browserHistory = useHistory();
   const user = useSelector((state) => state.users.find((user) => user.id.toString() === id));
 
   if (!user) {
     return null;
   }
 
-  // css
-  const paddedDivStyle = {
-    padding: 5,
+  const handleBlogClick = ({ id }) => (event) => {
+    event.preventDefault();
+    browserHistory.push(`/blogs/${id}`);
   };
 
   return (
-    <div style={paddedDivStyle}>
-      <h2>{user.name}</h2>
-      <h3 style={paddedDivStyle}>added blogs</h3>
-      <ul style={paddedDivStyle}>
-        {
-          user.blogs.map((blog) => (
-            <li key={blog.id}>
-              <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
+    <Card variant="outlined" className={classes.root}>
+      <CardHeader
+        title={user.name}
+        subheader={user.username}
+        titleTypographyProps={{ variant: 'h3' }}
+        subheaderTypographyProps={{ variant: 'h5' }}
+      />
+      <CardContent>
+        <Card variant="outlined">
+          <CardHeader title="Blogs Added" />
+          <CardContent>
+            <List>
+              {
+                user.blogs.map((blog) => (
+                  <ListItem alignItems="flex-start" button onClick={handleBlogClick(blog)} key={blog.id}>
+                    <ListItemText
+                      primary={(
+                        <Typography variant="h5">{blog.title}</Typography>
+                      )}
+                      secondary={` by ${blog.author}`}
+                    />
+                  </ListItem>
+                ))
+              }
+            </List>
+          </CardContent>
+        </Card>
+      </CardContent>
+    </Card>
   );
 };
 
